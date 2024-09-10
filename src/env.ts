8cf0c0196df0d1130f26da039095261b91ecdba4
Érico Marshall <ericogmarshall1@gmail.com>
2024-09-10 20:23:25 -0300

    initial commit.

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
-import z from "zod";
+import z from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-});
+  JWT_SECRET: z.string(),
+})
 
-export const env = envSchema.parse(process.env);
+export const env = envSchema.parse(process.env)
