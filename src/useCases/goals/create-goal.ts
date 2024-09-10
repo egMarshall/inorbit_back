f38641518e4a4dcde402edfef4546d45f9b06d6a
Érico Marshall <ericogmarshall1@gmail.com>
2024-09-10 20:26:02 -0300

    [FEAT] Adds goals useCases.

---
+++ src/useCases/goals/create-goal.ts
@@ -0,0 +1,29 @@
+import { db } from '../../db'
+import { goals } from '../../db/schema'
+
+interface CreateGoalRequest {
+  title: string
+  desiredWeeklyFrequency: number
+  userId: string
+}
+
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+  userId,
+}: CreateGoalRequest) {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+      userId,
+    })
+    .returning()
+
+  const goal = result[0]
+
+  return {
+    goal,
+  }
+}
